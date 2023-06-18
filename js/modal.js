c06460bf100fa3fedf8a9b5bb718c3aa1a30f3ee
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-06-18 12:57:07 +0200

    end bem

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,6 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("visually-hidden");
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
